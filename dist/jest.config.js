3fa9efb1ef0d7c853e530e9d51a072ea4536160e
boyssgoof <Boyssgoof@outlook.com>
2025-04-03 00:59:46 +0100

    updates Dockerfile

--- dist/jest.config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/** @type {import('ts-jest').JestConfigWithTsJest} **/
-module.exports = {
-    testEnvironment: "node",
-    globalSetup: "./jest.setup.ts",
-    transform: {
-        "^.+.tsx?$": ["ts-jest", {}],
-    },
-};
